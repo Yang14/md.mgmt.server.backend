14e9a353a8a82b131d6ea7fc10193b3581f6d1ac
Yang14 <359097729@qq.com>
2016-01-14 16:36:18 +0800

    impl getService & getService test

---
+++ src/main/java/md/mgmt/service/impl/GetMdAttrServiceImpl.java
@@ -0,0 +1,33 @@
+package md.mgmt.service.impl;
+
+import md.mgmt.base.md.MdAttr;
+import md.mgmt.dao.RdbDao;
+import md.mgmt.service.GetMdAttrService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by Mr-yang on 16-1-14.
+ */
+@Component
+public class GetMdAttrServiceImpl implements GetMdAttrService {
+
+    private static Logger logger = LoggerFactory.getLogger(GetMdAttrServiceImpl.class);
+
+    @Autowired
+    private RdbDao rdbDao;
+
+    @Override
+    public MdAttr getFileMdAttr(String fileCode) {
+        return rdbDao.getFileMdAttr(fileCode);
+    }
+
+    @Override
+    public List<MdAttr> getDirMdAttrList(long distrCode) {
+        return rdbDao.getDirMdAttrList(distrCode);
+    }
+}
