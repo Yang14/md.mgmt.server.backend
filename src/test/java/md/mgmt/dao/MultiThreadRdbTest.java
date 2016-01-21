72a79f0243b470721a22c876e595256c9ecfc598
Yang14 <359097729@qq.com>
2016-01-21 21:05:12 +0800

    test origin rdb perf in multi thread.

--- src/test/java/md/mgmt/dao/MultiThreadRdbTest.java
+++ src/test/java/md/mgmt/dao/MultiThreadRdbTest.java
@@ -2,6 +2,7 @@ package md.mgmt.dao;
 
 import com.alibaba.fastjson.JSON;
 import md.mgmt.base.md.MdAttr;
+import md.mgmt.connPool.ConnectionPoolImpl;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
@@ -15,9 +16,16 @@ public class MultiThreadRdbTest implements Runnable {
     private static Logger logger = LoggerFactory.getLogger(MultiThreadRdbTest.class);
 
     private static String path = "/data/test/conn";
+    private static int idGenerator = 0;
+    private final String threadName;
 
     static {
         RocksDB.loadLibrary();
+    }
+
+    public MultiThreadRdbTest() {
+        threadName = "pool-client-" + (++idGenerator);
+        Thread.currentThread().setName(threadName);
     }
 
     public void putObj(String key, Object obj) {
@@ -54,6 +62,32 @@ public class MultiThreadRdbTest implements Runnable {
 
     @Override
     public void run() {
+        for (int i = 0; i < 10; i++) {
+            this.putObj(Thread.currentThread().getName(), getMdAttr());
+        }
+        for (int i = 0; i < 10; i++) {
+            logger.info(i + Thread.currentThread().getName() + "-----" +this.getMdAttr(Thread.currentThread().getName()).toString());
+        }
+    }
 
+    public static void main(String[] args) throws Exception {
+        ConnectionPoolImpl poolImpl = new ConnectionPoolImpl();
+        poolImpl.setMinSize(1);
+        poolImpl.setMaxSize(100);
+        poolImpl.setDbPath("/data/test/conn");
+        poolImpl.setDebug(false);
+
+        for (int i=0;i<10;i++){
+            new Thread(new MultiThreadRdbTest()).start();
+        }
+    }
+
+    public static MdAttr getMdAttr() {
+        MdAttr mdAttr = new MdAttr();
+        mdAttr.setName("backend-fileCode");
+        mdAttr.setSize(7878);
+        mdAttr.setName("backend.t");
+        mdAttr.setAcl((short) 777);
+        return mdAttr;
     }
 }
